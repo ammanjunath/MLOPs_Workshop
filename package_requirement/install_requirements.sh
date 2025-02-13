90a7945afa66decb678a9eedbcceda67f2040f6b
ammanjunath <manjunathtek@gmail.com>
2025-02-13 12:46:22 +0530

    Update install_requirements.sh with installs for Ubuntu Latest

--- package_requirement/install_requirements.sh
+++ package_requirement/install_requirements.sh
@@ -3,3 +3,12 @@ python --version
 pip install --upgrade azure-cli
 pip install --upgrade azureml-sdk
 pip install -r requirements.txt
+sudo apt-get install build-essential
+pip install scikit-learn
+pip install joblib
+pip install numpy
+pip install pandas
+pip install pytest
+pip install pytest-cov
+pip install pytest-xdist
+pip install lightgbm
